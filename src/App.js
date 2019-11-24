8c2088fe79b6eb8e4f3a5147688ff28238713f54
Ahmad yar meo <ahmadyarmeo@gmail.com>
2019-11-24 22:15:42 +0500

    complete basic layouts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Tabs} from 'antd';
+import InPlay from './components/InPlay';
+import Upcoming from './components/UpComing';
 import './App.css';
+import 'antd/dist/antd.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { TabPane } = Tabs;
+
+class App extends Component{
+   constructor(props){
+     super(props);
+   }
+
+    callback = (key) => {
+        console.log(key);
+    };
+
+   render(){
+     return(
+         // Main
+         <Tabs defaultActiveKey="1" onChange={this.callback}>
+             <TabPane tab="In Play" key="1">
+                 <InPlay/>
+             </TabPane>
+             <TabPane tab="Up Coming" key="2">
+                 <Upcoming/>
+             </TabPane>
+         </Tabs>
+     )
+   }
 }
+
 
 export default App;
---
+++ src/components/UpComing.js
@@ -0,0 +1,226 @@
+import React, {Component} from 'react';
+
+class UpComing extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return(
+            <div id="upcoming">
+                <button className="collapsible" type="button">
+                    <span className="time italic">10/31 23:00</span>
+                    -
+                    <span className="league-name italic">ITF W25 Tucson</span>
+                    <span className="home bold">Sophia Whittle</span>
+                    <span> vs </span>
+                    <span className="away bold">Karina Miller</span>
+                </button>
+                <div>
+                    <table>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <colgroup></colgroup>
+                        <thead>
+                        <tr>
+                            <th>Hero / Opponent</th>
+                            <th>oBC</th>
+                            <th>oW1</th>
+                            <th>oB</th>
+                            <th>oR</th>
+                            <th>RESULT</th>
+                            <th>hR</th>
+                            <th>hB</th>
+                            <th>hW1</th>
+                            <th>hBC</th>
+                            <th>1</th>
+                            <th>2</th>
+                            <th>3</th>
+                            <th>4</th>
+                            <th>5</th>
+                            <th>6</th>
+                            <th>7</th>
+                            <th>8</th>
+                            <th>9</th>
+                            <th>10</th>
+                            <th>11</th>
+                            <th>12</th>
+                            <th>13</th>
+                            <th>14</th>
+                            <th>15</th>
+                            <th>16</th>
+                            <th>17</th>
+                            <th>18</th>
+                            <th>19</th>
+                            <th>20</th>
+                            <th>21</th>
+                            <th>22</th>
+                            <th>23</th>
+                            <th>24</th>
+                            <th>25</th>
+                            <th>26</th>
+                            <th>27</th>
+                            <th>28</th>
+                            <th>29</th>
+                            <th>30</th>
+                            <th>31</th>
+                            <th>32</th>
+                            <th>33</th>
+                            <th>34</th>
+                            <th>35</th>
+                            <th>36</th>
+                            <th>37</th>
+                            <th>38</th>
+                            <th>39</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td className="home bold">
+                                Sophia Whittle
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="graycell text-black">Dalayna Hewitt</td>
+                            <td>33%</td>
+                            <td>74%</td>
+                            <td>2</td>
+                            <td className="text-red">XXX</td>
+                            <td className="text-red">2-6,6-4,10-7</td>
+                            <td>XXX</td>
+                            <td>3</td>
+                            <td>63%</td>
+                            <td>43%</td>
+                            <td className="graycell">15</td>
+                            <td className="graycell bg-green">40</td>
+                            <td className="graycell">0</td>
+                            <td className="graycell">40</td>
+                            <td className="graycell">0</td>
+                            <td className="graycell">0</td>
+                            <td className="graycell">30</td>
+                            <td className="graycell bg-green">30</td>
+                            <td className="graycell bg-red">15</td>
+                            <td className="graycell">40</td>
+                            <td className="graycell">40</td>
+                            <td className="graycell">30</td>
+                            <td className="graycell bg-red">30</td>
+                            <td className="graycell bg-green">15</td>
+                            <td className="graycell">0</td>
+                            <td className="graycell">40</td>
+                            <td className="graycell">40</td>
+                            <td className="graycell">15</td>
+                            <td className="graycell">10-7</td>
+                        </tr>
+                        <tr>
+                            <td className="away bold">
+                                Karina Miller
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="graycell text-black">Malaika Rapolu</td>
+                            <td>67%</td>
+                            <td>44%</td>
+                            <td>2</td>
+                            <td className="text-red">XXX</td>
+                            <td className="text-red">7-5,5-7,5-10</td>
+                            <td>XXX</td>
+                            <td>3</td>
+                            <td>47%</td>
+                            <td>64%</td>
+                            <td className="graycell">15</td>
+                            <td className="graycell">30</td>
+                            <td className="graycell">15</td>
+                            <td className="graycell">40</td>
+                            <td className="graycell">40</td>
+                            <td className="graycell">0</td>
+                            <td className="graycell bg-green">30</td>
+                            <td className="graycell bg-red">40</td>
+                            <td className="graycell">15</td>
+                            <td className="graycell">30</td>
+                            <td className="graycell">15</td>
+                            <td className="graycell bg-green">30</td>
+                            <td className="graycell bg-red">40</td>
+                            <td className="graycell">30</td>
+                            <td className="graycell bg-red">40</td>
+                            <td className="graycell">30</td>
+                            <td className="graycell">15</td>
+                            <td className="graycell">40</td>
+                            <td className="graycell">40</td>
+                            <td className="graycell">30</td>
+                            <td className="graycell">15</td>
+                            <td className="graycell">30</td>
+                            <td className="graycell">15</td>
+                            <td className="graycell">15</td>
+                            <td className="graycell">0</td>
+                            <td className="graycell">30</td>
+                            <td className="graycell">40</td>
+                            <td className="graycell">15</td>
+                            <td className="graycell">30</td>
+                            <td className="graycell">15</td>
+                            <td className="graycell">30</td>
+                            <td className="graycell">15</td>
+                            <td className="graycell">15</td>
+                            <td className="graycell">0</td>
+                            <td className="graycell">30</td>
+                            <td className="graycell">40</td>
+                            <td className="graycell">15</td>
+                            <td className="graycell">30</td>
+                            <td className="graycell">40</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UpComing;
